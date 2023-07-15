5e68f361bdcfc787cd51d5650c20e0b9906c90a0
Henry-kiyimba <kiyimbahenry2222@gmail.com>
2023-07-15 13:18:56 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 /**
 * main - Entry point
 *
-* Return: 0 (SUCCESS)
+* Return: Always 0 (SUCCESS)
 */
-
 int main(void)
 {
-         printf("with proper grammar, but the outcome is a piece of art,\n");
-         return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
