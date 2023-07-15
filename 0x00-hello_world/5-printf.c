a8121422e9448c18f5b23af731577577a0a5879e
Henry-kiyimba <kiyimbahenry2222@gmail.com>
2023-07-15 13:07:35 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-
+#include <stdio
 /**
- * main - Entry point
- * This program conforms to the betty documentation style.
- * Description: 'This program prints a line of text to the screen'
- * Return: 0 (SUCCESS)
- **/
+* main - Entry point
+*
+* Return: 0 (SUCCESS)
+*/
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+         printf("with proper grammar, but the outcome is a piece of art,\n");
+         return (0);
 }
